a61ba96178d2c2c7820afad9f5fb2201e37bb667
AmanWorku <amexworku2014@gmail.com>
2023-01-20 17:19:40 +0300

    Add components and pages

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import BooksPage from './pages/BooksPage';
+import Categories from './pages/Categories';
+
+function App() {
+  return (
+    <div>
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<BooksPage />} />
+        <Route path="/categories" element={<Categories />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/DisplayBooks.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Book from './Book';
+
+const DisplayBooks = () => (
+  <>
+    <h1>Books</h1>
+    <Book />
+  </>
+);
+
+export default DisplayBooks;
