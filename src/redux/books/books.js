bc50a87f402c64d53179ef4f18a7ab6f17b38578
AmanWorku <amexworku2014@gmail.com>
2023-01-24 20:13:33 +0300

    Add redux folder and files

---
+++ src/redux/books/books.js
@@ -0,0 +1,17 @@
+import { createReducer } from '@reduxjs/toolkit';
+
+const ADD = 'bookstore/books/ADD';
+const DELETE = 'bookstore/books/DELETE';
+
+const bookReducer = createReducer([], {
+  [ADD]: (state, action) => ([...state, action.book]),
+},
+{ [DELETE]: (state, action) => state.filter((book) => book.id !== action.book.id) });
+
+export const addBook = (book) => (
+  { type: ADD, book }
+);
+
+export const deleteBook = (book) => (
+  { type: DELETE, book }
+);
