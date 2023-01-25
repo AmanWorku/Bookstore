67ca4ac29b9e5497d53325762c7285e30e526cb3
AmanWorku <amexworku2014@gmail.com>
2023-01-25 08:47:21 +0300

    Fix linter errors

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit';
 import bookReducer from './books/books';
-import categoryReducer from './categories/Categories';
+import categoryReducer from './categories/categories';
 
 const store = configureStore({
   reducer: {
