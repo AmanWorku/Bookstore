d4714db45863cf3710082b26282553359b2cf190
AmanWorku <amexworku2014@gmail.com>
2023-01-25 15:01:04 +0300

    Make sample books able to be displayed on the page

--- src/components/Book.js
+++ src/components/Book.js
@@ -1,12 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const Book = () => (
-  <div className="book list">
-    <h1>
-      <span className="title">Book Title</span>
-      <span className="author">Book Author</span>
-    </h1>
-    <button type="submit" className="remove-btn">Remove</button>
-  </div>
-);
+const Book = () => {
+  const books = useSelector((state) => state.books);
+  return (
+
+    <div className="book list">
+      <h1>
+        <span className="title">Book Title</span>
+        <span className="author">Book Author</span>
+      </h1>
+      {
+        books.map((book) => (
+          <>
+            <h3>{book.author}</h3>
+            <h3>{book.title}</h3>
+          </>
+        ))
+      }
+      <button type="submit" className="remove-btn">Remove</button>
+    </div>
+  );
+};
 export default Book;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,16 +1,16 @@
 const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
 const INITIAL_STATE = [
-    {
-        id: 1,
-        title: 'Harry Potter',
-        author: 'J.K. Rowling'
-    },
-    {
-        id: 2,
-        title: 'Jurassic Park',
-        author: 'J.K. Rowling'
-    }
+  {
+    id: 1,
+    title: 'Harry Potter',
+    author: 'J.K. Rowling',
+  },
+  {
+    id: 2,
+    title: 'Jurassic Park',
+    author: 'J.K. Rowling',
+  },
 ];
 const bookReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
