401c1c21842942e5cf03980b2dd0c7cb9f4e3440
AmanWorku <amexworku2014@gmail.com>
2023-01-25 14:40:35 +0300

    Add sample books

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,6 +1,17 @@
 const ADD = 'bookstore/books/ADD';
 const REMOVE = 'bookstore/books/REMOVE';
-const INITIAL_STATE = 0;
+const INITIAL_STATE = [
+    {
+        id: 1,
+        title: 'Harry Potter',
+        author: 'J.K. Rowling'
+    },
+    {
+        id: 2,
+        title: 'Jurassic Park',
+        author: 'J.K. Rowling'
+    }
+];
 const bookReducer = (state = INITIAL_STATE, action) => {
   switch (action.type) {
     case ADD:
