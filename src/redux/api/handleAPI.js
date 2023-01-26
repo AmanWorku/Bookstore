1a1e38bae3295319e152631fdf53cdfc7c528f83
AmanWorku <amexworku2014@gmail.com>
2023-01-26 13:00:59 +0300

    Create functions with createAsyncThunk . for creating deleting and fetching

--- src/redux/api/handleAPI.js
+++ src/redux/api/handleAPI.js
@@ -4,23 +4,23 @@ const BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/boo
 const API_ID = 'KijtxrzWmwD49GPxRSbw';
 
 const httpRequest = axios.create({
-    baseURL: BASE_URL,
-    headers: {'Content-Type': 'application/json'}, 
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
 });
 
 const fetchBooks = () => httpRequest.get(`/apps/${API_ID}/books`);
 
 const create = ({
-    id,author,title,category,
-}) => httpRequest.post(`/apps/${API_ID}/books`,{
-    item_id: id,
-    author,
-    title,
-    category
+  id, author, title, category,
+}) => httpRequest.post(`/apps/${API_ID}/books`, {
+  item_id: id,
+  author,
+  title,
+  category,
 });
 
-const remove = (id) => httpRequest.delete(`/apps/${API_ID}/books`);
+const remove = (id) => httpRequest.delete(`/apps/${API_ID}/books/${id}`);
 
-const handleAPI = { fetchbooks, create, remove };
+const handleAPI = { fetchBooks, create, remove };
 
 export default handleAPI;
--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,23 +1,55 @@
-const ADD = 'bookstore/books/ADD';
-const REMOVE = 'bookstore/books/REMOVE';
-const INITIAL_STATE = [];
-const bookReducer = (state = INITIAL_STATE, action) => {
-  switch (action.type) {
-    case ADD:
-      return [...state, action.book];
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import handleAPI from '../api/handleAPI';
 
-    case REMOVE:
-      return state.filter((book) => book.id !== action.bookId);
-    default:
-      return state;
+const createBook = createAsyncThunk('bookstore/books/create', async (book) => {
+  const {
+    id, author, title, category,
+  } = book;
+
+  try {
+    await handleAPI.create({
+      id, author, title, category,
+    });
+    return book;
+  } catch (err) {
+    return err.message;
   }
-};
-export const addBook = (book) => (
-  { type: ADD, book }
-);
+});
 
-export const removeBook = (bookId) => (
-  { type: REMOVE, bookId }
-);
+const deleteBook = createAsyncThunk('bookstore/books/delete', async (id) => {
+  await handleAPI.remove(id);
+  return {id};
+});
 
-export default bookReducer;
+const fetchAllBooks = createAsyncThunk('bookstore/books/getAll', async () => {
+  try {
+    const result = await handleAPI.fetchBooks();
+    return result.data;
+  }catch (err) {
+    return err.message;
+  }
+})
+
+// const ADD = 'bookstore/books/ADD';
+// const REMOVE = 'bookstore/books/REMOVE';
+// const INITIAL_STATE = [];
+// const bookReducer = (state = INITIAL_STATE, action) => {
+//   switch (action.type) {
+//     case ADD:
+//       return [...state, action.book];
+
+//     case REMOVE:
+//       return state.filter((book) => book.id !== action.bookId);
+//     default:
+//       return state;
+//   }
+// };
+// export const addBook = (book) => (
+//   { type: ADD, book }
+// );
+
+// export const removeBook = (bookId) => (
+//   { type: REMOVE, bookId }
+// );
+
+// export default bookReducer;
