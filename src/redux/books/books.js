9bd43181eea2b2b0580728c57ca8ec2dfce475a2
AmanWorku <amexworku2014@gmail.com>
2023-01-26 13:53:15 +0300

    Remove commented code section

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -61,27 +61,3 @@ const bookSlice = createSlice({
 
 const { reducer } = bookSlice;
 export default reducer;
-
-// const ADD = 'bookstore/books/ADD';
-// const REMOVE = 'bookstore/books/REMOVE';
-// const INITIAL_STATE = [];
-// const bookReducer = (state = INITIAL_STATE, action) => {
-//   switch (action.type) {
-//     case ADD:
-//       return [...state, action.book];
-
-//     case REMOVE:
-//       return state.filter((book) => book.id !== action.bookId);
-//     default:
-//       return state;
-//   }
-// };
-// export const addBook = (book) => (
-//   { type: ADD, book }
-// );
-
-// export const removeBook = (bookId) => (
-//   { type: REMOVE, bookId }
-// );
-
-// export default bookReducer;
