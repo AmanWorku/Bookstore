73ae82a700930e43399c07c66221905f872a13cc
AmanWorku <amexworku2014@gmail.com>
2023-02-02 14:40:11 +0300

    Add files for the progress part

---
+++ src/components/BookProgress.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import CircleProgress from './CircleProgress';
+import Stat from './Stat';
+
+const BookProgress = ({ progress }) => {
+  const { completed, currentChapter } = progress;
+  return (
+    <div className="progress-container">
+      <CircleProgress />
+      <Stat percent={completed} />
+      <div className="progress-divider" />
+      <div className="current-chapter-container">
+        <div>
+          <p className="current-chapter-label">CURRENT CHAPTER</p>
+          <p className="current-chapter">{currentChapter}</p>
+        </div>
+        <div>
+          <button className="primary-button" type="button">UPDATE PROGRESS</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+BookProgress.propTypes = {
+  progress: PropTypes.shape({
+    currentChapter: PropTypes.string.isRequired,
+    completed: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default BookProgress;
