05aa33588570aa8776ec174b5fffda21936635bf
AmanWorku <amexworku2014@gmail.com>
2023-02-02 08:51:25 +0300

    Add styling for the navigation bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,9 +3,14 @@ import { Link } from 'react-router-dom';
 
 const NavBar = () => (
   <nav>
-    <Link to="/">Books</Link>
-    <Link to="/categories">Categories</Link>
-    <span className="material-icons primary-color">person</span>
+    <h1 className="page-name">Bookstore CMS</h1>
+    <ul className="nav-links">
+      <li><Link to="/" className="nav-link active-link">BOOKS</Link></li>
+      <li><Link to="/categories" className="nav-link ">CATEGORIES</Link></li>
+    </ul>
+    <button type="button" className="account-btn">
+      <span className="material-icons primary-color">person</span>
+    </button>
   </nav>
 );
 export default NavBar;
